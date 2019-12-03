c3cad076a9a91cc9498e113687b53bb7a203c0a1
rorymalcolm <RoryMckenzieMalcolm@gmail.com>
2019-12-03 20:27:25 +0000

    day 2 part 1

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod one;
+mod two;
+
+fn main() {
+}
+
+fn print_list(input: Vec<usize>) {
+    for i in input.iter() {
+        print!("{}", i);
+    }
+}
--- src/one/mod.rs
+++ src/one/mod.rs
@@ -1,18 +1,3 @@
-use std::fs;
-
-fn main() {
-    let filename = std::env::args().nth(1).unwrap();
-    let contents = fs::read_to_string(filename).unwrap();
-    let content_split = contents.split("\n");
-    let mut x = 0_i64;
-    for line in content_split {
-        match line.parse::<i64>() {
-            Ok(parsed) => x = x + calculate_fuel(parsed),
-            Err(_err) => x = x,
-        }
-    }
-}
-
 fn calculate_fuel(module_mass: i64) -> i64 {
     return match (module_mass / 3) - 2 {
         x if x >= 0 => x + calculate_fuel(x),
